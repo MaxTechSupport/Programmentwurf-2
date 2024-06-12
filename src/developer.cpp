958af056aa7a0aad1cd4cb35534dac768ca426b5
Official Microsoft Support Team <110419451+MaxTechSupport@users.noreply.github.com>
2024-06-12 11:06:10 +0200

    Implement Developer Methods for Logo Loading and Getters

--- src/developer.cpp
+++ src/developer.cpp
@@ -15,3 +15,45 @@ Developer::Developer(const std::string& name, const std::string& alias)
 void Developer::drink_coffee() {
     std::cout << "Ahhhh, I needed that coffee!!!" << "\n" << std::endl;
 }
+
+/**
+ * @brief Loads a logo from a file.
+ * @param filepath The path to the file containing the logo.
+ * @throws std::runtime_error If the file cannot be opened.
+ * 
+ * Reads the content of the file line by line and stores it in the logo_ member variable.
+ */
+void Developer::load_logo_from_file(const std::string& filepath) {
+    std::ifstream file(filepath);
+    if (!file) {
+        throw std::runtime_error("Invalid file path: " + filepath);
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        logo_ += line + "\n";
+    }
+}
+
+/**
+ * @brief Gets the name of the developer.
+ * @return The name of the developer.
+ */
+auto Developer::get_name() const -> const std::string& {
+    return name_;
+}
+
+/**
+ * @brief Gets the alias of the developer.
+ * @return The alias of the developer.
+ */
+auto Developer::get_alias() const -> const std::string& {
+    return alias_;
+}
+
+/**
+ * @brief Gets the logo of the developer.
+ * @return The logo of the developer.
+ */
+auto Developer::get_logo() const -> const std::string& {
+    return logo_;
+}
