d9ccfd77a17deade357b455933f7e4d9e87e05be
Official Microsoft Support Team <110419451+MaxTechSupport@users.noreply.github.com>
2024-06-14 11:56:15 +0200

    Add Main File with Developer Includes and Basic Setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "../include/developer.hpp"
+#include "../include/junior_developer.hpp"
+#include "../include/senior_developer.hpp"
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+auto main() -> int {
+    
+    return 0;
+}
